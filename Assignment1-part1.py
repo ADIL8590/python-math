50da9fcdf8fb447a1007519532782ae5e9795602
ADIL8590 <mash8851@gmail.com>
2026-01-30 15:17:55 +0530

    Assignment1 Tutedude added

---
+++ Assignment1-part1.py
@@ -0,0 +1,12 @@
+"""Writting a code for addition, subtraction , multiplication and division of
+two numbers 'a' and 'b'"""
+a=int(input("Enter the first number : "))
+b=int(input("Enter the second number : "))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("Addition  : ", addition)
+print("Subtraction : ", subtraction)
+print("Multiplication  : ", multiplication)
+print("Division  : ", division)
---
+++ Assignment1-partii.py
@@ -0,0 +1,4 @@
+#code for writing personalised greeting message
+name1=input("Enter your first name : ")
+name2=input("Enter your last name : ")
+print("Hello,", name1, name2,"! Welcome to the  Python program")
