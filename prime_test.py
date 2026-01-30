045e4a6e90692c4fce32821e35bbd7cbffefb67f
ADIL8590 <mash8851@gmail.com>
2026-01-30 14:33:43 +0530

    added code 2 for primility test

--- prime_test.py
+++ prime_test.py
@@ -1,3 +1,5 @@
+# Checking if a number is prime using recursion code1
+
 def is_prime(n, i=2):
     if (n<=2):
         return True if n==2 else False
@@ -7,3 +9,14 @@ def is_prime(n, i=2):
         return True
     return is_prime(n, i+1)
 print(is_prime(29))
+# Checking if a number is prime using recursion code2
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+print(is_prime(23))
+
+
