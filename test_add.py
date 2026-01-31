7944a2d434e1a0dfc7f59843a4fe3ce6cd365a62
ADIL8590 <64102877+ADIL8590@users.noreply.github.com>
2026-01-31 12:01:53 +0530

    def addition added

--- test_add.py
+++ test_add.py
@@ -1,10 +1,13 @@
 # sum of numbers
+def addition(a,b):
+    return a+b
 def test_addition_basic(a,b):
     assert addition(2,3)==5
     assert addition(-1,1)==0
     assert addition(0,0)==0
 if _name_=="_main_":
     print(addition(2,3))
+
 
 
 
