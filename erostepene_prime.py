f9611e9f281e23dfac250c90836d22cf8aa1c529
ADIL8590 <64102877+ADIL8590@users.noreply.github.com>
2026-02-01 00:08:21 +0530

    Update erostepene_prime.py

--- erostepene_prime.py
+++ erostepene_prime.py
@@ -3,7 +3,7 @@ def erostephene_prime(n):
     primes = [True for i in range(n+1)]
     p = 2
     while (p*p <= n):
-        if (primes[p]==True):
+        if (primes[p]):
             for i in range(p*p, n+1, p):
                 primes[i] = False
         p += 1
