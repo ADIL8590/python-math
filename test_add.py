968bdf913f3c8d80005b25f25822d4df23c41f24
ADIL8590 <64102877+ADIL8590@users.noreply.github.com>
2026-01-31 11:57:18 +0530

    Fix syntax errors

--- test_add.py
+++ test_add.py
@@ -1,10 +1,11 @@
 # sum of numbers
 def test_addition_basic(a,b):
     assert addition(2,3)==5
-    assert addition(-1,1)=0
-    assert addition(0,0)=0
+    assert addition(-1,1)==0
+    assert addition(0,0)==0
 if _name_=="_main_":
     print(addition(2,3))
+
 
 
 
