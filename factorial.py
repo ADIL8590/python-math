a849dc626629cae52be1ee0ff91fdadd39ae8853
ADIL8590 <mash8851@gmail.com>
2026-02-11 10:00:27 +0530

    factorial edited

--- factorial.py
+++ factorial.py
@@ -1,9 +1,14 @@
+#finding factorial of a number using recursion by user input
+n=int(input("Enter a number: "))
 def fact(n):
     if (n==0 or n==1):
         return 1
     else:
         return  fact(n-1)*n
-    
-
- 
-print(fact(6))
+print(f"Factorial of {n} is {fact(n)}")
+# Finding factorial of a number using loop by user input
+n=int(input("Enter a number to find its factorial: "))
+factorial = 1
+for i in range(1, n+1):
+    factorial *= i
+print(f"The factorial of {n} is: {factorial}")
