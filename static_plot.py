b22024d15fe619111054ba772df310ec0e920fbd
ADIL8590 <mash8851@gmail.com>
2026-02-11 11:31:20 +0530

    static plot updated

--- static_plot.py
+++ static_plot.py
@@ -1,7 +1,7 @@
 #static_plot.py
 import matplotlib.pyplot as plt
 #data
-data_list = [60,58, 48, 52, 44, 56, 45, 37, 58, 49, 50]
+data_list = [60,58, 48, 52, 44, 56, 45]
 #create figure and axis objects
 fig, ax = plt.subplots()
 #plot data
@@ -9,7 +9,7 @@ ax.plot(data_list, marker='o', linestyle='-', color='b')
 #add title and labels
 ax.set_xlabel('Day Number')
 ax.set_ylabel('Temperature (°F)')
-ax.set_title('Temperature Over 11 Days')
+ax.set_title('Temperature Over 7 Days')
 #save and show plot
 plt.savefig('temperature_plot.png')
 plt.show() 
